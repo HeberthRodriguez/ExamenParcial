a5f1ae864ad6062245fd207c5920b467bbed8cb2
HeberthRodriguez <heberthrodriguez10@gmail.com>
2025-05-09 11:45:24 -0600

    cambio gidhud INSTRUCCIONES.java

--- INSTRUCCIONES.java
+++ INSTRUCCIONES.java
@@ -1,4 +1,5 @@
 // crea una clase persona le ´pones los atributos, los gets, sets, constructores
 // creas las clases testigoConfidencial y testigo.
 // en la clase GUI termina la ventana de menu con chatgpt solamente decile que estas heredando la clase GUI de JFrame y que layouts estas usando
+// hola
 
