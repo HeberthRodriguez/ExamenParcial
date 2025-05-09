963bc04adaadfbd35f26e3307cda945523b0f68b
Heberth Torres Rodriguez <heberthrodriguez10@gmail.com>
2025-05-09 01:18:43 -0600

    INSTRUCCIONES

---
+++ INSTRUCCIONES.java
@@ -0,0 +1,4 @@
+// crea una clase persona le ´pones los atributos, los gets, sets, constructores
+// creas las clases testigoConfidencial y testigo.
+// en la clase GUI termina la ventana de menu con chatgpt solamente decile que estas heredando la clase GUI de JFrame y que layouts estas usando
+
