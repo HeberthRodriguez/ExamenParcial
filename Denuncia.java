4e03b0968e28e2a9d12948f612e879a6948e6b8e
Heberth Torres Rodriguez <heberthrodriguez10@gmail.com>
2025-05-08 23:47:14 -0600

    SISTEMAS DE DENUNCIAS actualizacion 1

---
+++ Denuncia.java
@@ -0,0 +1,59 @@
+
+public class Denuncia {
+
+    private String tipo;
+    private String descripcion;
+    private String lugar;
+    private String fecha;
+   
+    public Denuncia(String tipo, String descripcion, String lugar, String fecha) {
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+        this.lugar = lugar;
+        this.fecha = fecha;
+    }
+
+    public String toString() {
+       
+        return "Tipo: " + tipo + "\nDescripción: " + descripcion + "\nLugar: " + lugar + "\nFecha: " + fecha + "\n";
+
+    }
+    
+    
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+}
